4d7cffdba9c67cb593161c66aed3dba7a1fb1bd3
liasse-clb <162380208+liasse-clb@users.noreply.github.com>
2025-11-24 14:22:56 -0300

    ajeitando um problema do deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import App from './App.tsx'
 import './index.css'
 import "flowbite"
-import Servicos from './components/ui/services.tsx'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
